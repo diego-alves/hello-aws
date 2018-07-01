499f73e8e4d0edc848d2c327b0f289e81ecd20ec
Diego Alves <dalves@uolinc.com>
2018-07-01 16:12:03 -0300

    first-parent

--- tag.sh
+++ tag.sh
@@ -3,7 +3,7 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-git tag v$(git describe --tags | grep -oP "\d+\.\d+.\d+" | awk -F. '{print $1"."$2"."$3+1}')
+git tag v$(git describe --tags --first-parent | grep -oP "\d+\.\d+.\d+" | awk -F. '{print $1"."$2"."$3+1}')
 
 git remote set-url origin https://${GH_TOKEN}@github.com/diego-alves/hello-aws.git
 git push --tags
