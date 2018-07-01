39b3a4298f7360077f7bc22283fa36a18c54dc1f
Diego Alves <dalves@uolinc.com>
2018-07-01 15:42:18 -0300

    push tag

--- tag.sh
+++ tag.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+
 git tag v$(git describe --tags | grep -oP "\d+\.\d+.\d+" | awk -F. '{print $1"."$2"."$3+1}')
-git push --tags
+
+git remote add origin-tags https://${GH_TOKEN}@github.com/diego-alves/hello-aws.git > /dev/null 2>&1
+git push --quiet --set-upstream origin-tags master
